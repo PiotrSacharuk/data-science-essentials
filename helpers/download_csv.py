bc5e5d1d5cbca44afa73a5d2f4eacee8ea99c1be
Piotr Sacharuk <piotr.sacharuk@gmail.com>
2025-09-18 11:44:37 +0200

    More secure code for download_csv

--- helpers/download_csv.py
+++ helpers/download_csv.py
@@ -1,8 +1,11 @@
 import urllib.request
 
 url = "https://archive.ics.uci.edu/ml/machine-learning-databases/iris/iris.data"
-response = urllib.request.urlopen(url)
-data = response.read()
+try:
+    response = urllib.request.urlopen(url, timeout=10)
+    data = response.read()
 
-with open("iris.csv", "wb") as f:
-    f.write(data)
+    with open("iris.csv", "wb") as f:
+        f.write(data)
+except Exception as e:
+    print(f"Error downloading or saving the file: {e}")
